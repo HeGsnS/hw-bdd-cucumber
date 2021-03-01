fb03879f23011773dc885a4939b967e6794b79a0
EC2 Default User <ec2-user@ip-172-31-32-111.us-east-2.compute.internal>
2021-03-01 09:00:33 +0000

    Complete Part 3.

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,6 +1,10 @@
 class Movie < ActiveRecord::Base
-  
+  # all_ratings method
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+  # with_ratings method 
+    def self.with_ratings(ratings)
+        where("LOWER(rating) IN (?)", ratings.map(&:downcase))
+    end
 end
